be17ea7cadb9751e32308dc12d91e2bc321c1288
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-04 08:06:40 +0530

    added Calculator Fake support

---
+++ lib/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color.fromRGBO(13, 20, 24,1);
+const textColor = Color.fromRGBO(241, 241, 242, 1);
+const appBarColor = Color.fromRGBO(42,47,50,1);
+const messageColor = Colors.deepPurple;
+const searchBarColor = Color.fromRGBO(50, 55, 57, 1);
