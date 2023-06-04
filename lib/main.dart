7827d4f20618183b26f455b88cffd385e03f4216
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-04 15:19:31 +0530

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:p_chat/calculator/widgets/calc_page.dart';
 import 'package:p_chat/colors.dart';
+import 'package:p_chat/firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
