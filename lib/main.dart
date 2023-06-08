9208f798e3c232e5910c3ace34f2afd167bfd6b9
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-08 11:56:52 +0530

    added calc auth and mobile layout screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:p_chat/apps/auth/screens/auth.dart';
 import 'package:p_chat/apps/calculator/screens/calc_page.dart';
 import 'package:p_chat/colors.dart';
 import 'package:p_chat/firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -26,12 +27,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
-        if (snapshot.hasData){
-          return CalcPage();
-        }
-        return AuthScreen();
-      },),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return CalcPage();
+          }
+          return AuthScreen();
+        },
+      ),
     );
   }
 }
