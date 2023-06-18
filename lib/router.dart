ed4d5bb0143dc9778a373c7d484a2dbadb124270
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-18 11:36:09 +0530

    added

--- lib/router.dart
+++ lib/router.dart
@@ -1,16 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:whatsapp_ui/common/widgets/error.dart';
+import 'package:p_chat/apps/auth/screens/profile.dart';
+import 'package:p_chat/common/widgets/error.dart';
 import 'package:p_chat/apps/auth/screens/auth.dart';
-import 'package:p_chat/apps/auth/screens/otp_screen.dart';
-import 'package:p_chat/apps/auth/screens/user_information_screen.dart';
-import 'package:p_chat/apps/group/screens/create_group_screen.dart';
-import 'package:p_chat/apps/select_contacts/screens/select_contacts_screen.dart';
+// import 'package:p_chat/apps/auth/screens/otp_screen.dart';
+// import 'package:p_chat/apps/auth/screens/user_information_screen.dart';
+// import 'package:p_chat/apps/group/screens/create_group_screen.dart';
+// import 'package:p_chat/apps/select_contacts/screens/select_contacts_screen.dart';
 import 'package:p_chat/apps/chat/screens/mobile_chat_screen.dart';
-import 'package:p_chat/apps/status/screens/confirm_status_screen.dart';
-import 'package:p_chat/apps/status/screens/status_screen.dart';
-import 'package:whatsapp_ui/models/status_model.dart';
+// import 'package:p_chat/apps/status/screens/confirm_status_screen.dart';
+// import 'package:p_chat/apps/status/screens/status_screen.dart';
+// import 'package:p_chat/models/status_model.dart';
+
+import 'apps/global_contacts/screens/global_contacts.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -18,53 +19,46 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(
         builder: (context) => const AuthScreen(),
       );
-    case OTPScreen.routeName:
-      final verificationId = settings.arguments as String;
+    case ProfileScreen.routeName:
       return MaterialPageRoute(
-        builder: (context) => OTPScreen(
-          verificationId: verificationId,
-        ),
+        builder: (context) => const ProfileScreen(),
       );
-    case UserInformationScreen.routeName:
+    case GlobalContactsScreen.routeName:
       return MaterialPageRoute(
-        builder: (context) => const UserInformationScreen(),
-      );
-    case SelectContactsScreen.routeName:
-      return MaterialPageRoute(
-        builder: (context) => const SelectContactsScreen(),
+        builder: (context) => const GlobalContactsScreen(),
       );
     case MobileChatScreen.routeName:
       final arguments = settings.arguments as Map<String, dynamic>;
       final name = arguments['name'];
       final uid = arguments['uid'];
-      final isGroupChat = arguments['isGroupChat'];
-      final profilePic = arguments['profilePic'];
+      // final isGroupChat = arguments['isGroupChat'];
+      // final profilePic = arguments['profilePic'];
       return MaterialPageRoute(
         builder: (context) => MobileChatScreen(
           name: name,
           uid: uid,
-          isGroupChat: isGroupChat,
-          profilePic: profilePic,
+          // isGroupChat: isGroupChat,
+          // profilePic: profilePic,
         ),
       );
-    case ConfirmStatusScreen.routeName:
-      final file = settings.arguments as File;
-      return MaterialPageRoute(
-        builder: (context) => ConfirmStatusScreen(
-          file: file,
-        ),
-      );
-    case StatusScreen.routeName:
-      final status = settings.arguments as Status;
-      return MaterialPageRoute(
-        builder: (context) => StatusScreen(
-          status: status,
-        ),
-      );
-    case CreateGroupScreen.routeName:
-      return MaterialPageRoute(
-        builder: (context) => const CreateGroupScreen(),
-      );
+    // case ConfirmStatusScreen.routeName:
+    //   final file = settings.arguments as File;
+    //   return MaterialPageRoute(
+    //     builder: (context) => ConfirmStatusScreen(
+    //       file: file,
+    //     ),
+    //   );
+    // case StatusScreen.routeName:
+    //   final status = settings.arguments as Status;
+    //   return MaterialPageRoute(
+    //     builder: (context) => StatusScreen(
+    //       status: status,
+    //     ),
+    //   );
+    // case CreateGroupScreen.routeName:
+    //   return MaterialPageRoute(
+    //     builder: (context) => const CreateGroupScreen(),
+    //   );
     default:
       return MaterialPageRoute(
         builder: (context) => const Scaffold(
