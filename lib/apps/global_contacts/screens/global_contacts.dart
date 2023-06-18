7e3a7ac6da7504fef970d80bee5fa9938ab38c0c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-18 10:17:37 +0530

    added

---
+++ lib/apps/global_contacts/screens/global_contacts.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GlobalContactsScreen extends ConsumerWidget {
+  static const String routeName = '/global-contact';
+  const GlobalContactsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select contact'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+          ),
+        ],
+      ),
+      body: ref.watch(getContactsProvider).when(
+            data: (contactList) => ListView.builder(
+                itemCount: contactList.length,
+                itemBuilder: (context, index) {
+                  final contact = contactList[index];
+                  return InkWell(
+                    onTap: () => selectContact(ref, contact, context),
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: ListTile(
+                        title: Text(
+                          contact.displayName,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        leading: contact.photo == null
+                            ? null
+                            : CircleAvatar(
+                                backgroundImage: MemoryImage(contact.photo!),
+                                radius: 30,
+                              ),
+                      ),
+                    ),
+                  );
+                }),
+            error: (err, trace) => ErrorScreen(error: err.toString()),
+            loading: () => const Loader(),
+          ),
+    );
+  }
+  }
