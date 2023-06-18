653ba3c71251525611db937fee476d875443df20
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-18 10:17:30 +0530

    added

--- lib/common/screens/mobile_layout_screen.dart
+++ lib/common/screens/mobile_layout_screen.dart
@@ -1,26 +1,67 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:p_chat/apps/auth/screens/auth.dart';
 import 'package:p_chat/apps/auth/screens/profile.dart';
-import 'package:p_chat/colors.dart';
+import 'package:p_chat/common/utils/colors.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:p_chat/common/utils/colors.dart';
+import 'package:p_chat/common/utils/utils.dart';
+import 'package:p_chat/apps/auth/controller/auth_controller.dart';
+// import 'package:whatsapp_ui/features/group/screens/create_group_screen.dart';
+import 'package:p_chat/apps/global_contacts/screens/global_contacts.dart';
+// import 'package:p/features/chat/widgets/contacts_list.dart';
+// import 'package:whatsapp_ui/features/status/screens/confirm_status_screen.dart';
+// import 'package:whatsapp_ui/features/status/screens/status_contacts_screen.dart';
 
-class MobileLayoutScreen extends StatefulWidget {
+class MobileLayoutScreen extends ConsumerStatefulWidget {
   const MobileLayoutScreen({Key? key}) : super(key: key);
 
   @override
-  State<MobileLayoutScreen> createState() => _MobileLayoutScreenState();
+  ConsumerState<MobileLayoutScreen> createState() => _MobileLayoutScreenState();
 }
 
-class _MobileLayoutScreenState extends State<MobileLayoutScreen> {
+class _MobileLayoutScreenState extends ConsumerState<MobileLayoutScreen> with WidgetsBindingObserver, TickerProviderStateMixin {
   Future logout() async {
     await FirebaseAuth.instance.signOut();
 
+  }
+    late TabController tabBarController;
+  @override
+  void initState() {
+    super.initState();
+    tabBarController = TabController(length: 3, vsync: this);
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.resumed:
+        ref.read(authControllerProvider).setUserState(true);
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+      case AppLifecycleState.paused:
+        ref.read(authControllerProvider).setUserState(false);
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         backgroundColor: appBarColor,
         title: Text(
           "Chat",
@@ -38,6 +79,32 @@ class _MobileLayoutScreenState extends State<MobileLayoutScreen> {
       body: Center(child: ElevatedButton(child: Text('Mobile!'), onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => ProfileScreen()));
       },),
-    ));
+      
+    ),
+    floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            // if (tabBarController.index == 0) {
+            //   
+            // } else {
+            //   File? pickedImage = await pickImageFromGallery(context);
+            //   if (pickedImage != null) {
+            //     Navigator.pushNamed(
+            //       context,
+            //       ConfirmStatusScreen.routeName,
+            //       arguments: pickedImage,
+            //     );
+            //   }
+            // }
+            Navigator.pushNamed(context, GlobalContactsScreen.routeName);
+          },
+          backgroundColor: tabColor,
+          child: const Icon(
+            Icons.comment,
+            color: Colors.white,
+          ),
+      )
+    
+    );
+    
   }
 }
