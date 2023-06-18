9ba9344c9192635b473de8c481053603617babf2
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-18 11:29:54 +0530

    added

--- lib/apps/global_contacts/repository/global_contacts_repository.dart
+++ lib/apps/global_contacts/repository/global_contacts_repository.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:whatsapp_ui/common/utils/utils.dart';
-import 'package:whatsapp_ui/models/user_model.dart';
-import 'package:whatsapp_ui/features/chat/screens/mobile_chat_screen.dart';
+import 'package:p_chat/apps/chat/screens/mobile_chat_screen.dart';
+import 'package:p_chat/models/user.dart';
+import 'package:p_chat/common/utils/utils.dart';
+import 'package:p_chat/models/user.dart';
+import 'package:p_chat/apps/chat/screens/mobile_chat_screen.dart';
 
 final selectContactsRepositoryProvider = Provider(
   (ref) => SelectContactRepository(
@@ -19,50 +22,58 @@ class SelectContactRepository {
     required this.firestore,
   });
 
-  Future<List<Contact>> getContacts() async {
-    List<Contact> contacts = [];
-    try {
-      if (await FlutterContacts.requestPermission()) {
-        contacts = await FlutterContacts.getContacts(withProperties: true);
-      }
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-    return contacts;
+  Future<QuerySnapshot<Map<String, dynamic>>> getContacts() async {
+    var userCollection = await firestore.collection('users').get();
+    return userCollection;
   }
 
-  void selectContact(Contact selectedContact, BuildContext context) async {
-    try {
-      var userCollection = await firestore.collection('users').get();
-      bool isFound = false;
+  // void selectContact(Contact selectedContact, BuildContext context) async {
+  //   try {
+  //     var userCollection = await firestore.collection('users').get();
+  //     bool isFound = false;
 
-      for (var document in userCollection.docs) {
-        var userData = UserModel.fromMap(document.data());
-        String selectedPhoneNum = selectedContact.phones[0].number.replaceAll(
-          ' ',
-          '',
-        );
-        if (selectedPhoneNum == userData.phoneNumber) {
-          isFound = true;
-          Navigator.pushNamed(
+  //     for (var document in userCollection.docs) {
+  //       var userData = UserModel.fromMap(document.data());
+  //       String selectedPhoneNum = selectedContact.phones[0].number.replaceAll(
+  //         ' ',
+  //         '',
+  //       );
+  //       if (selectedPhoneNum == userData.phoneNumber) {
+  //         isFound = true;
+  //         Navigator.pushNamed(
+  //           context,
+  //           MobileChatScreen.routeName,
+  //           arguments: {
+  //             'name': userData.name,
+  //             'uid': userData.uid,
+  //           },
+  //         );
+  //       }
+  //     }
+  // if (!isFound) {
+  //       showSnackBar(
+  //         context: context,
+  //         content: 'This number does not exist on this app.',
+  //       );
+  //     }
+  //   } catch (e) {
+  //     showSnackBar(context: context, content: e.toString());
+  //   }
+  // }
+    void selectContact(UserModel selectedUserData, BuildContext context) async{
+      // var userCollection = await firestore.collection('users').get();
+      // for (var document in userCollection.docs) {
+      //   var userData = UserModel.fromMap(document.data());
+      // }
+      Navigator.pushNamed(
             context,
             MobileChatScreen.routeName,
             arguments: {
-              'name': userData.name,
-              'uid': userData.uid,
+              'name': selectedUserData.name,
+              'uid': selectedUserData.uid,
             },
           );
-        }
-      }
-
-      if (!isFound) {
-        showSnackBar(
-          context: context,
-          content: 'This number does not exist on this app.',
-        );
-      }
-    } catch (e) {
-      showSnackBar(context: context, content: e.toString());
     }
-  }
+
+      
 }
