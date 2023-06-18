e4786146c7cd54ee98c33c264614f3777c322d63
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-18 10:56:43 +0530

    added

---
+++ lib/apps/chat/screens/mobile_chat_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MobileChatScreen extends ConsumerWidget {
+  const MobileChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Placeholder();
+  }
+}
