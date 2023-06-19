051bf4c06c35938ca62bbfbd4a32a1785d259c6f
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-19 17:07:44 +0530

    added

--- lib/apps/chat/widgets/message_cards/sender_message_card.dart
+++ lib/apps/chat/widgets/message_cards/sender_message_card.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:swipe_to/swipe_to.dart';
 
-import 'package:whatsapp_ui/common/utils/colors.dart';
-import 'package:whatsapp_ui/common/enums/message_enum.dart';
-import 'package:whatsapp_ui/features/chat/widgets/display_text_image_gif.dart';
+import 'package:p_chat/common/utils/colors.dart';
+import 'package:p_chat/common/enums/message_enum.dart';
+// import 'package:p_chat/apps/chat/widgets/display_text_image_gif.dart';
 
 class SenderMessageCard extends StatelessWidget {
   const SenderMessageCard({
