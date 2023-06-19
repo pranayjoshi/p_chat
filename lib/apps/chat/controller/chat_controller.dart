9ba3bb132da680d660a487be9e3a0cafb66abcdc
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-19 13:48:13 +0530

    added

--- lib/apps/chat/controller/chat_controller.dart
+++ lib/apps/chat/controller/chat_controller.dart
@@ -1,7 +1,9 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:p_chat/apps/auth/controller/auth_controller.dart';
+import 'package:p_chat/apps/chat/repository/chat_repository.dart';
 // import 'package:whatsapp_ui/common/enums/message_enum.dart';
 // import 'package:whatsapp_ui/common/providers/message_reply_provider.dart';
 // import 'package:whatsapp_ui/features/auth/controller/auth_controller.dart';
@@ -10,114 +12,114 @@
 // import 'package:whatsapp_ui/models/group.dart';
 // import 'package:whatsapp_ui/models/message.dart';
 
-// final chatControllerProvider = Provider((ref) {
-//   final chatRepository = ref.watch(chatRepositoryProvider);
-//   return ChatController(
-//     chatRepository: chatRepository,
-//     ref: ref,
-//   );
-// });
+final chatControllerProvider = Provider((ref) {
+  final chatRepository = ref.watch(chatRepositoryProvider);
+  return ChatController(
+    chatRepository: chatRepository,
+    ref: ref,
+  );
+});
 
-// class ChatController {
-//   final ChatRepository chatRepository;
-//   final ProviderRef ref;
-//   ChatController({
-//     required this.chatRepository,
-//     required this.ref,
-//   });
+class ChatController {
+  final ChatRepository chatRepository;
+  final ProviderRef ref;
+  ChatController({
+    required this.chatRepository,
+    required this.ref,
+  });
 
-//   Stream<List<ChatContact>> chatContacts() {
-//     return chatRepository.getChatContacts();
-//   }
+  // Stream<List<ChatContact>> chatContacts() {
+  //   return chatRepository.getChatContacts();
+  // }
 
-//   Stream<List<Group>> chatGroups() {
-//     return chatRepository.getChatGroups();
-//   }
+  // Stream<List<Group>> chatGroups() {
+  //   return chatRepository.getChatGroups();
+  // }
 
-//   Stream<List<Message>> chatStream(String recieverUserId) {
-//     return chatRepository.getChatStream(recieverUserId);
-//   }
+  // Stream<List<Message>> chatStream(String recieverUserId) {
+  //   return chatRepository.getChatStream(recieverUserId);
+  // }
 
-//   Stream<List<Message>> groupChatStream(String groupId) {
-//     return chatRepository.getGroupChatStream(groupId);
-//   }
+  // Stream<List<Message>> groupChatStream(String groupId) {
+  //   return chatRepository.getGroupChatStream(groupId);
+  // }
 
-//   void sendTextMessage(
-//     BuildContext context,
-//     String text,
-//     String recieverUserId,
-//     bool isGroupChat,
-//   ) {
-//     final messageReply = ref.read(messageReplyProvider);
-//     ref.read(userDataAuthProvider).whenData(
-//           (value) => chatRepository.sendTextMessage(
-//             context: context,
-//             text: text,
-//             recieverUserId: recieverUserId,
-//             senderUser: value!,
-//             messageReply: messageReply,
-//             isGroupChat: isGroupChat,
-//           ),
-//         );
-//     ref.read(messageReplyProvider.state).update((state) => null);
-//   }
+  void sendTextMessage(
+    BuildContext context,
+    String text,
+    String recieverUserId,
+    // bool isGroupChat,
+  ) {
+    // final messageReply = ref.read(messageReplyProvider);
+    ref.read(userDataAuthProvider).whenData(
+          (value) => chatRepository.sendTextMessage(
+            context: context,
+            text: text,
+            recieverUserId: recieverUserId,
+            senderUser: value!,
+            // messageReply: messageReply,
+            // isGroupChat: isGroupChat,
+          ),
+        );
+    // ref.read(messageReplyProvider.state).update((state) => null);
+  }
 
-//   void sendFileMessage(
-//     BuildContext context,
-//     File file,
-//     String recieverUserId,
-//     MessageEnum messageEnum,
-//     bool isGroupChat,
-//   ) {
-//     final messageReply = ref.read(messageReplyProvider);
-//     ref.read(userDataAuthProvider).whenData(
-//           (value) => chatRepository.sendFileMessage(
-//             context: context,
-//             file: file,
-//             recieverUserId: recieverUserId,
-//             senderUserData: value!,
-//             messageEnum: messageEnum,
-//             ref: ref,
-//             messageReply: messageReply,
-//             isGroupChat: isGroupChat,
-//           ),
-//         );
-//     ref.read(messageReplyProvider.state).update((state) => null);
-//   }
+  // void sendFileMessage(
+  //   BuildContext context,
+  //   File file,
+  //   String recieverUserId,
+  //   MessageEnum messageEnum,
+  //   bool isGroupChat,
+  // ) {
+  //   final messageReply = ref.read(messageReplyProvider);
+  //   ref.read(userDataAuthProvider).whenData(
+  //         (value) => chatRepository.sendFileMessage(
+  //           context: context,
+  //           file: file,
+  //           recieverUserId: recieverUserId,
+  //           senderUserData: value!,
+  //           messageEnum: messageEnum,
+  //           ref: ref,
+  //           messageReply: messageReply,
+  //           isGroupChat: isGroupChat,
+  //         ),
+  //       );
+  //   ref.read(messageReplyProvider.state).update((state) => null);
+  // }
 
-//   void sendGIFMessage(
-//     BuildContext context,
-//     String gifUrl,
-//     String recieverUserId,
-//     bool isGroupChat,
-//   ) {
-//     final messageReply = ref.read(messageReplyProvider);
-//     int gifUrlPartIndex = gifUrl.lastIndexOf('-') + 1;
-//     String gifUrlPart = gifUrl.substring(gifUrlPartIndex);
-//     String newgifUrl = 'https://i.giphy.com/media/$gifUrlPart/200.gif';
+  // void sendGIFMessage(
+  //   BuildContext context,
+  //   String gifUrl,
+  //   String recieverUserId,
+  //   bool isGroupChat,
+  // ) {
+  //   final messageReply = ref.read(messageReplyProvider);
+  //   int gifUrlPartIndex = gifUrl.lastIndexOf('-') + 1;
+  //   String gifUrlPart = gifUrl.substring(gifUrlPartIndex);
+  //   String newgifUrl = 'https://i.giphy.com/media/$gifUrlPart/200.gif';
 
-//     ref.read(userDataAuthProvider).whenData(
-//           (value) => chatRepository.sendGIFMessage(
-//             context: context,
-//             gifUrl: newgifUrl,
-//             recieverUserId: recieverUserId,
-//             senderUser: value!,
-//             messageReply: messageReply,
-//             isGroupChat: isGroupChat,
-//           ),
-//         );
-//     ref.read(messageReplyProvider.state).update((state) => null);
-//   }
+  //   ref.read(userDataAuthProvider).whenData(
+  //         (value) => chatRepository.sendGIFMessage(
+  //           context: context,
+  //           gifUrl: newgifUrl,
+  //           recieverUserId: recieverUserId,
+  //           senderUser: value!,
+  //           messageReply: messageReply,
+  //           isGroupChat: isGroupChat,
+  //         ),
+  //       );
+  //   ref.read(messageReplyProvider.state).update((state) => null);
+  // }
 
-//   void setChatMessageSeen(
-//     BuildContext context,
-//     String recieverUserId,
-//     String messageId,
-//   ) {
-//     chatRepository.setChatMessageSeen(
-//       context,
-//       recieverUserId,
-//       messageId,
-//     );
-//   }
-// }
+  void setChatMessageSeen(
+    BuildContext context,
+    String recieverUserId,
+    String messageId,
+  ) {
+    chatRepository.setChatMessageSeen(
+      context,
+      recieverUserId,
+      messageId,
+    );
+  }
+}
