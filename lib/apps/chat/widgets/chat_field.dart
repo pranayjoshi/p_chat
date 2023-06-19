455a4309ca0c09f41285f5af07932dcdb3830099
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-19 16:07:16 +0530

    added

--- lib/apps/chat/widgets/chat_field.dart
+++ lib/apps/chat/widgets/chat_field.dart
@@ -10,7 +10,6 @@ import 'package:p_chat/common/utils/colors.dart';
 import 'package:p_chat/common/enums/message_enum.dart';
 // import 'package:whatsapp_ui/common/providers/message_reply_provider.dart';
 import 'package:p_chat/common/utils/utils.dart';
-import 'package:whatsapp_ui/features/chat/controller/chat_controller.dart';
 // import 'package:whatsapp_ui/features/chat/widgets/message_reply_preview.dart';
 
 class BottomChatField extends ConsumerStatefulWidget {
@@ -57,30 +56,31 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
             context,
             _messageController.text.trim(),
             widget.recieverUserId,
-            widget.isGroupChat,
+            // widget.isGroupChat,
           );
       setState(() {
         _messageController.text = '';
       });
-    } else {
-      var tempDir = await getTemporaryDirectory();
-      var path = '${tempDir.path}/flutter_sound.aac';
-      if (!isRecorderInit) {
-        return;
-      }
-      if (isRecording) {
-        await _soundRecorder!.stopRecorder();
-        sendFileMessage(File(path), MessageEnum.audio);
-      } else {
-        await _soundRecorder!.startRecorder(
-          toFile: path,
-        );
-      }
-
-      setState(() {
-        isRecording = !isRecording;
-      });
     }
+    // else {
+    //   var tempDir = await getTemporaryDirectory();
+    //   var path = '${tempDir.path}/flutter_sound.aac';
+    //   if (!isRecorderInit) {
+    //     return;
+    //   }
+    //   if (isRecording) {
+    //     await _soundRecorder!.stopRecorder();
+    //     sendFileMessage(File(path), MessageEnum.audio);
+    //   } else {
+    //     await _soundRecorder!.startRecorder(
+    //       toFile: path,
+    //     );
+    //   }
+
+    //   setState(() {
+    //     isRecording = !isRecording;
+    //   });
+    // }
   }
 
   // void sendFileMessage(
@@ -190,14 +190,18 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                       child: Row(
                         children: [
                           IconButton(
-                            onPressed: toggleEmojiKeyboardContainer,
+                            // onPressed: toggleEmojiKeyboardContainer,
+                            onPressed:() {
+                              
+                            },
                             icon: const Icon(
                               Icons.emoji_emotions,
                               color: Colors.grey,
                             ),
                           ),
                           IconButton(
-                            onPressed: selectGIF,
+                            // onPressed: selectGIF,
+                            onPressed: (){},
                             icon: const Icon(
                               Icons.gif,
                               color: Colors.grey,
@@ -213,14 +217,14 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         IconButton(
-                          onPressed: selectImage,
+                          onPressed: (){},
                           icon: const Icon(
                             Icons.camera_alt,
                             color: Colors.grey,
                           ),
                         ),
                         IconButton(
-                          onPressed: selectVideo,
+                          onPressed: (){},
                           icon: const Icon(
                             Icons.attach_file,
                             color: Colors.grey,
