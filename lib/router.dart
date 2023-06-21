4e3c0fe0546c8dbcc80151101d85126f05bb0d52
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-21 06:29:13 +0530

    added

--- lib/router.dart
+++ lib/router.dart
@@ -45,13 +45,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       final name = arguments['name'];
       final uid = arguments['uid'];
       // final isGroupChat = arguments['isGroupChat'];
-      // final profilePic = arguments['profilePic'];
+      final profilePic = arguments['profilePic'];
       return MaterialPageRoute(
         builder: (context) => MobileChatScreen(
           name: name,
           uid: uid,
           // isGroupChat: isGroupChat,
-          // profilePic: profilePic,
+          profilePic: profilePic,
         ),
       );
     // case ConfirmStatusScreen.routeName:
