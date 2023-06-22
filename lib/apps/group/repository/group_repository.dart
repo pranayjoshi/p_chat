fb7e6497f3d448d1fe26e6bf7f9db4004ec91c31
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-22 23:32:16 +0530

    added

---
+++ lib/apps/group/repository/group_repository.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final groupRepositoryProvider = Provider(
+  (ref) => GroupRepository(
+    firestore: FirebaseFirestore.instance,
+    auth: FirebaseAuth.instance,
+    ref: ref,
+  ),
+);
+
+class GroupRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+  final ProviderRef ref;
+
+  GroupRepository(
+      {required this.firestore, required this.auth, required this.ref});
+}
--- lib/apps/group/screens/create_group_screen.dart
+++ lib/apps/group/screens/create_group_screen.dart
@@ -22,6 +22,70 @@ class _CreateGroupScreenState extends ConsumerState<CreateGroupScreen> {
         backgroundColor: appBarColor,
         iconTheme: IconThemeData(color: textColor),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Stack(
+              children: [
+                image == null
+                    ? const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          'https://png.pngitem.com/pimgs/s/649-6490124_katie-notopoulos-katienotopoulos-i-write-about-tech-round.png',
+                        ),
+                        radius: 64,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(
+                          image!,
+                        ),
+                        radius: 64,
+                      ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: selectImage,
+                    icon: const Icon(
+                      Icons.add_a_photo,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                controller: groupNameController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Group Name',
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(8),
+              child: const Text(
+                'Select Contacts',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SelectContactsGroup(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createGroup,
+        backgroundColor: tabColor,
+        child: const Icon(
+          Icons.done,
+          color: Colors.white,
+        ),
+      ),
+    );
     );
   }
 }
