775d1b10555f8ba2a5eade09f1c7571a4d544d4c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-22 20:58:34 +0530

    added

--- lib/apps/group/controller/group_controller.dart
+++ lib/apps/group/controller/group_controller.dart
@@ -2,14 +2,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final groupRepositoryProvider = Provider((ref) => GroupRepository);
-
+final groupRepositoryProvider = Provider(
+  (ref) => GroupRepository(
+    firestore: FirebaseFirestore.instance,
+    auth: FirebaseAuth.instance,
+    ref: ref,
+  ),
+);
 
 class GroupRepository {
   final FirebaseFirestore firestore;
   final FirebaseAuth auth;
   final ProviderRef ref;
 
-  GroupRepository(this.firestore, this.auth, this.ref);
-
+  GroupRepository(
+      {required this.firestore, required this.auth, required this.ref});
 }
