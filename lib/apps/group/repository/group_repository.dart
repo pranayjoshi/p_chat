31e02aff0ee387e5740063c3658e13e669494937
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-23 00:21:45 +0530

    added

--- lib/apps/group/repository/group_repository.dart
+++ lib/apps/group/repository/group_repository.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:p_chat/common/utils/utils.dart';
+import 'package:p_chat/models/group.dart' as model;
 
 final groupRepositoryProvider = Provider(
   (ref) => GroupRepository(
@@ -17,4 +24,48 @@ class GroupRepository {
 
   GroupRepository(
       {required this.firestore, required this.auth, required this.ref});
+
+    void createGroup(BuildContext context, String name, File profilePic,
+      List<Contact> selectedContact) async {
+    try {
+      List<String> uids = [];
+      for (int i = 0; i < selectedContact.length; i++) {
+        var userCollection = await firestore
+            .collection('users')
+            .where(
+              'phoneNumber',
+              isEqualTo: selectedContact[i].phones[0].number.replaceAll(
+                    ' ',
+                    '',
+                  ),
+            )
+            .get();
+
+        if (userCollection.docs.isNotEmpty && userCollection.docs[0].exists) {
+          uids.add(userCollection.docs[0].data()['uid']);
+        }
+      }
+      var groupId = const Uuid().v1();
+      
+      final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('/group/$groupId',);
+        await storageRef.putFile(profilePic);
+      String profileUrl = await storageRef.getDownloadURL();
+      model.Group group = model.Group(
+        senderId: auth.currentUser!.uid,
+        name: name,
+        groupId: groupId,
+        lastMessage: '',
+        groupPic: profileUrl,
+        membersUid: [auth.currentUser!.uid, ...uids],
+        timeSent: DateTime.now(),
+      );
+
+      await firestore.collection('groups').doc(groupId).set(group.toMap());
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}
 }
