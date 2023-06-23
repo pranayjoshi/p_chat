7fe881a403f4f92f918fb6043bcb675aa92daba3
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-23 14:23:59 +0530

    added

---
+++ lib/apps/group/controller/group_controller.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:p_chat/models/chat_contact.dart';
+
+
+import '../repository/group_repository.dart';
+
+final groupControllerProvider = Provider((ref) {
+  final groupRepository = ref.read(groupRepositoryProvider);
+  return GroupController(
+    groupRepository: groupRepository,
+    ref: ref,
+  );
+});
+
+class GroupController {
+  final GroupRepository groupRepository;
+  final ProviderRef ref;
+  GroupController({
+    required this.groupRepository,
+    required this.ref,
+  });
+
+  void createGroup(BuildContext context, String name, String profileUrl,
+      List<ChatContact> selectedContact) {
+    groupRepository.createGroup(context, name, profileUrl, selectedContact);
+  }
+}
