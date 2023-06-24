5f8dab429e95387cc431259d7998d245ab3e17ea
pranayjoshi <pranayjoshi4466@gmail.com>
2023-06-24 12:16:30 +0530

    added

--- lib/common/providers/message_reply_provider.dart
+++ lib/common/providers/message_reply_provider.dart
@@ -3,10 +3,11 @@ import 'package:p_chat/common/enums/message_enum.dart';
 
 class MessageReply {
   final String message;
+  final String name;
   final bool isMe;
   final MessageEnum messageEnum;
 
-  MessageReply(this.message, this.isMe, this.messageEnum);
+  MessageReply(this.message, this.name, this.isMe, this.messageEnum);
 }
 
 final messageReplyProvider = StateProvider<MessageReply?>((ref) => null);
